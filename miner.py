b2ebb7fc2bcfbb46f7e621699238f87de3569332
jinxjinx <jingxu086@gmail.com>
2018-06-18 00:28:21 -0400

    feat(toychain): add functions & classes

--- miner.py
+++ miner.py
@@ -1,1 +1,11 @@
 # coding=utf-8
+
+
+class Miner(threading.Thread):
+    def __init__(self, chain, tx_pool):
+        threading.Thread.__init__(self)
+        self.chain = chain
+        self.tx_pool = tx_pool
+
+    def run(self):
+        pass
--- toychain.py
+++ toychain.py
@@ -1,10 +1,56 @@
 # coding=utf-8
 
 
-chain = []
-tx_pool = []
-private_key = ""
+class ToyChain:
+    def __init__(self, pvt_key):
+        self.chain = []
+        self.tx_pool = []
+        self.pvt_key = pvt_key
 
 
 def hash():
+    pass
+
+
+def get_nonce():
+    pass
+
+
+def verify_chain():
+    pass
+
+
+def verify_tx():
+    pass
+
+
+def verify_block():
+    pass
+
+
+def resolve_conflicts():
+    pass
+
+
+def post_tx():
+    pass
+
+
+def post_block():
+    pass
+
+
+def post_node():
+    pass
+
+
+def new_block():
+    pass
+
+
+def new_tx():
+    pass
+
+
+def new_block():
     pass
